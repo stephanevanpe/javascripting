6bcb6f86c81e906b6c3a81a8fb1651f57aa14411
Stephane Van Pe <stephanevanpe@gmail.com>
2019-03-13 13:38:14 +0100

    end

--- Javascripting.js
+++ Javascripting.js
@@ -1,1 +1,11 @@
-let result = prompt("Hey mon ami ! Tu aimes ça les patates ?" [, default]);
+let result = prompt("Hey mon ami ! Tu aimes ça les patates ?" );
+let patate='#'
+if (result === 'oui') {
+    for (var i = 0; i < 7; i++) {
+        console.log(patate);
+        patate+=patate[i];
+    
+    }
+} else if (result === 'non'){
+    alert("Tu sais pas ce que tu perds");
+}else{ alert("Je vous sens comme tiraillé")};
